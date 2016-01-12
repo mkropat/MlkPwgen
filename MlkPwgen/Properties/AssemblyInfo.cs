f8b77fff2836bfc774b7ca816a08f168185efcaf
Michael Kropat <mail@michael.kropat.name>
2016-01-12 02:09:03 +0000

    Clean up project settings

--- MlkPwgen/Properties/AssemblyInfo.cs
+++ MlkPwgen/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MlkPwgen")]
-[assembly: AssemblyCopyright("Copyright © Michael Kropat 2015")]
+[assembly: AssemblyCopyright("Copyright © Michael Kropat 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+//[assembly: AssemblyFileVersion("0.0.0.0")]
